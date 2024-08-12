3759f72c53084bb36e6b8d79c15bb9a3b6e80d65
iinsue <hinsu93@naver.com>
2024-08-12 22:36:50 +0900

    다크 모드 추가

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const HomPage = () => {
   return (
-    <div>
+    <div className="flex h-full items-center justify-center">
       <UserButton afterSwitchSessionUrl="/" />
+      <ModeToggle />
     </div>
   );
 };
