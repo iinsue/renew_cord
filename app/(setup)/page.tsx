f3bf716253bd3afde566a3bfebfc113a7ba088f7
iinsue <hinsu93@naver.com>
2024-08-15 21:18:59 +0900

    프로필 정보 세팅 코드 추가

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,30 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { UserButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton afterSwitchSessionUrl="/" />
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default SetupPage;
