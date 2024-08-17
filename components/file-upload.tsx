03279daf77f019f23cf820180db015b18544ba60
iinsue <hinsu93@naver.com>
2024-08-17 21:13:59 +0900

    서버 이미지 업로드 구현

---
+++ components/file-upload.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative size-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+        >
+          <X className="size-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({});
+// 접근 가능한 라우트 목록
+const publicRoutes = createRouteMatcher(["/api/uploadthing"]);
+
+export default clerkMiddleware((auth, request) => {
+  // 접근 가능한 라우트 이외에는 로그인해야 접근 가능
+  if (!publicRoutes(request)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
