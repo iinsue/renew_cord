7f69f31339d36598659569449487b7d07b5dc27f
iinsue <hinsu93@naver.com>
2024-08-18 22:57:21 +0900

    서버 등록 기능 구현

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { FileUpload } from "../file-upload";
+
 import {
   Dialog,
   DialogContent,
@@ -22,7 +27,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileUpload } from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Server name is required." }),
@@ -30,6 +34,7 @@ const formSchema = z.object({
 });
 
 export const InitialModal = () => {
+  const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -47,7 +52,14 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
