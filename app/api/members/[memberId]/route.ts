461f4b3e79b6b60b13397c88424251ac8ad1229c
iinsue <hinsu93@naver.com>
2024-08-24 18:40:28 +0900

    서버 멤버 역할 변경 백엔드

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,65 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { memberId: string } },
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(request.url);
+    const { role } = await request.json();
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (!params.memberId) {
+      return new NextResponse("Member ID missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBERS_IS_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
